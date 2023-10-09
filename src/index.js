bda87481ee312e2b6daaa673594e216c7d026917
eric githaiga <ericgithaiga007@gmail.com>
2023-10-09 13:42:04 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 const init = () => {
+
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const id = event.target.id.value;
+
+        fetch(` http://localhost:3000/movies/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                const movieTitle = document.querySelector('#title');
+                movieTitle.textContent = response.title;
+                const movieSummary = document.querySelector('#summary');
+                movieSummary.textContent = response.summary;
+            })
+        
+    })
   
 }
 
